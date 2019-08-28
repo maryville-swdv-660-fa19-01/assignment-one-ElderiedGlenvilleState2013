ad511bcb8d0e93b846ca5d8510471880eb94ba35
Elderied McKinney <mckinneydemetrice@icloud.com>
2019-08-27 22:04:51 -0400

    added tensorflow

--- elderiedweek1.py
+++ elderiedweek1.py
@@ -1,8 +1,24 @@
 import tensorflow as tf
-
+import numpy as np
 
 h = tf.constant("Hello")
 w = tf.constant(" World!")
 hw=h+w
 with tf.Session() as sess: ans = sess.run(hw)
-print(ans)
+print (ans)
+
+x = tf.constant([1,2,3],name='x',dtype=tf.float32)
+print(x.dtype)
+x=tf.cast(x,tf.int64)
+print(x.dtype)
+
+
+
+sess = tf.InteractiveSession()
+c = tf.linspace(0.0, 4.0, 5)
+print("The content of 'c': \n {}\n".format(c.eval()))
+sess.close()
+
+a = tf.constant([[ 1,2,3], [4,5,6]])
+
+print(a.get_shape())
