b6313856be8fc36a1521e42f206320d43dbd5d19
Elderied McKinney <mckinneydemetrice@icloud.com>
2019-08-27 22:35:24 -0400

    added functions imported numpy
    imported numpy and tensorflow. I creared a couple variable using tensorflow and created a graph.

--- elderiedweek1.py
+++ elderiedweek1.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 import numpy as np
 
+
+
+
 h = tf.constant("Hello")
 w = tf.constant(" World!")
 hw=h+w
@@ -22,3 +25,27 @@ sess.close()
 a = tf.constant([[ 1,2,3], [4,5,6]])
 
 print(a.get_shape())
+
+x = tf.constant([1,0,1])
+print(x.get_shape())
+
+x = tf.expand_dims(x,1)
+print(x.get_shape())
+
+b = tf.matmul(a,x)
+
+sess = tf.InteractiveSession()
+print("matmul result: \n {}".format(b.eval()))
+sess.close()
+
+with tf.Graph().as_default():
+    c1 = tf.constant(4, dtype=tf.float64, name='c')
+    c2 = tf.constant(4, dtype=tf.int32, name='c')
+
+print(c1.name)
+print(c2.name)
+
+y_data = np.random.randn(5,10)
+w_data = np.random.randn(10,1)
+
+print(y_data,w_data)
